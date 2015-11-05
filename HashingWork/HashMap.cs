87e13a3aac019557a785844d93ec9df6b6543fdf
David Barnes <barnesdavidj@gmail.com>
2015-11-05 15:53:10 -0500

    Inital commit with all of the work finished.

---
+++ HashingWork/HashMap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingWork
+{
+    class HashMap
+    {
+        //Vars and constants needed
+        private const int PRIMEVALUE = 97;
+        int[] theKeys;
+        string[] theValues;
+
+        //Constructor
+        public HashMap()
+        {
+            theKeys = new int[PRIMEVALUE];
+            theValues = new string[PRIMEVALUE];
+        }
+
+        //overrided ToString method to print the entire hash table
+        public override string ToString()
+        {
+            string returnString = "Key     => Value" + Environment.NewLine;
+            returnString += "-------------------" + Environment.NewLine;
+            for (int i=0; i < theKeys.Length; i++)
+            {
+                if (theKeys[i] != 0)
+                {
+                    returnString += theKeys[i].ToString().PadRight(5) + " => " 
+                        + theValues[i] + Environment.NewLine;
+                }
+            }
+            return returnString;
+        }
+
+        //Show all the places in the array.
+        public string ShowArrays()
+        {
+            string returnString = "";
+            returnString += "----------------------";
+            for (int i=0; i< theKeys.Length; i++)
+            {
+                returnString += "[" + i + "] - " + theKeys[i] + " => " + theValues[i] +
+                    Environment.NewLine;
+            }
+            return returnString;
+        }
+
+        //Method to get a value out based on a key
+        public string Get(int Key)
+        {
+            int indexForValue = hashFunction(Key);
+            return theValues[indexForValue];
+        }
+
+        //Method to put a new value into the hashmap based on the key
+        public void Put(int Key, string Value)
+        {
+            int indexForValue = hashFunction(Key);
+            theKeys[indexForValue] = Key;
+            theValues[indexForValue] = Value;
+        }
+
+
+        //The magical hash function that does the mapping from a large number down to our prime size
+        private int hashFunction(int key)
+        {
+            return key % PRIMEVALUE;
+        }
+
+    }
+}
---
+++ HashingWork/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashMap valleyNumToName = new HashMap();
+            Dictionary<int, string> valleyNumToNameUsingDictionary = new Dictionary<int, string>();
+
+            Console.WriteLine("Adding some entries");
+            valleyNumToName.Put(12345, "James Kirk");
+            valleyNumToName.Put(23456, "Benjamin Sisko");
+            valleyNumToName.Put(34567, "Kathryn Janeway");
+            valleyNumToName.Put(45678, "Jean-Luc Picard");
+            valleyNumToName.Put(56789, "Jonathan Archer");
+
+            Console.WriteLine("The filled hash table");
+            Console.WriteLine(valleyNumToName);
+            Console.WriteLine();
+            Console.WriteLine(valleyNumToName.ShowArrays());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Get a single person out");
+            Console.WriteLine("valleyNumToName.Get(45678)");
+            Console.WriteLine(valleyNumToName.Get(45678));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Gonna put in two people that hash to the same location");
+            valleyNumToName.Put(26189, "First Entry");
+            valleyNumToName.Put(26092, "Second Entry");
+
+            Console.WriteLine(valleyNumToName);
+            Console.WriteLine();
+            Console.WriteLine(valleyNumToName.ShowArrays());
+
+            Console.WriteLine("******************************************************************");
+
+            Console.WriteLine("Adding some entries");
+            
+            valleyNumToNameUsingDictionary.Add(12345, "James Kirk");
+            valleyNumToNameUsingDictionary.Add(23456, "Benjamin Sisko");
+            valleyNumToNameUsingDictionary.Add(34567, "Kathryn Janeway");
+            valleyNumToNameUsingDictionary.Add(45678, "Jean-Luc Picard");
+            valleyNumToNameUsingDictionary.Add(56789, "Jonathan Archer");
+
+            Console.WriteLine("The filled hash table");
+            foreach (int key in valleyNumToNameUsingDictionary.Keys)
+            {
+                Console.WriteLine(key + " => " + valleyNumToNameUsingDictionary[key]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Get a single person out");
+            Console.WriteLine("valleyNumToName.Get(45678)");
+            Console.WriteLine(valleyNumToNameUsingDictionary[45678]);
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Gonna put in two people that hash to the same location");
+            valleyNumToNameUsingDictionary.Add(26189, "First Entry");
+            valleyNumToNameUsingDictionary.Add(26092, "Second Entry");
+
+            foreach (int key in valleyNumToNameUsingDictionary.Keys)
+            {
+                Console.WriteLine(key + " => " + valleyNumToNameUsingDictionary[key]);
+            }
+
+        }
+    }
+}
